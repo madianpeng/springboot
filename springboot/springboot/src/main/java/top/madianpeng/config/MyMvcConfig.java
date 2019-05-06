29865fcf2fb4873456f54f453f2585a6b6d82a87
madianpeng <madianpeng@qq.com>
2019-05-06 09:25:45 +0800

    creatproject

---
+++ springboot/springboot/src/main/java/top/madianpeng/config/MyMvcConfig.java
@@ -0,0 +1,21 @@
+package top.madianpeng.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class MyMvcConfig extends WebMvcConfigurationSupport {
+
+	@Override
+	protected void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/main").setViewName("index");
+	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+	}
+
+}
