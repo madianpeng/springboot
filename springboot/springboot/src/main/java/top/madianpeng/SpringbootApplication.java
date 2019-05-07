4a8d371d41f30e1a994fef97e311db687dc62c20
madianpeng <madianpeng@qq.com>
2019-05-07 12:17:13 +0800

    扫描mapper

--- springboot/springboot/src/main/java/top/madianpeng/SpringbootApplication.java
+++ springboot/springboot/src/main/java/top/madianpeng/SpringbootApplication.java
@@ -1,8 +1,10 @@
 package top.madianpeng;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("top.madianpeng.mapper")
 @SpringBootApplication
 public class SpringbootApplication {
 
