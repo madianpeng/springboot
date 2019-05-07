70f1113b7dd35ca08a835fcbfd514915b0b2282a
madianpeng <madianpeng@qq.com>
2019-05-07 11:20:00 +0800

    druid配置

--- springboot/springboot/src/main/java/top/madianpeng/config/MyMvcConfig.java
+++ springboot/springboot/src/main/java/top/madianpeng/config/MyMvcConfig.java
@@ -1,21 +1,30 @@
 package top.madianpeng.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import top.madianpeng.interceptor.LoginHandlerInterceptor;
 
 @Configuration
 public class MyMvcConfig extends WebMvcConfigurationSupport {
 
 	@Override
 	protected void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/login").setViewName("user/login");
 		registry.addViewController("/main").setViewName("index");
 	}
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 	}
 
+	/*
+	 * @Override protected void addInterceptors(InterceptorRegistry registry) {
+	 * registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/*")
+	 * .excludePathPatterns("/tologin","/user/login","/druid/*"); }
+	 */
 }
